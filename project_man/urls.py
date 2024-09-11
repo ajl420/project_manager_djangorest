663668d8d88287618a29f1471af9cbf16dafbc7a
ajl420 <ajl35353535@gmail.com>
2024-09-11 14:56:25 +0300

    Addind: README.MD

--- project_man/urls.py
+++ project_man/urls.py
@@ -19,7 +19,8 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-from app.models import Task
+from app.models import Task, Project, Priority, Status
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Episyche Technologies",
@@ -30,6 +31,9 @@ schema_view = get_schema_view(
 )
 
 admin.site.register(Task)
+admin.site.register(Project)
+admin.site.register(Priority)
+admin.site.register(Status)
 urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0),name='schema-swagger-ui'),
     path('api/', include("app.urls"),name='api'),
