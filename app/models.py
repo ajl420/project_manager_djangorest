840ab0a250e868942fe9bee8649637babc217fd2
ajl420 <ajl35353535@gmail.com>
2024-09-11 06:40:56 +0300

    Create: Models(Status,Priority,Project,Task)

--- app/models.py
+++ app/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+class Status(models.Model):
+    label = models.CharField(max_length=100, null=False)
+
+class Priority(models.Model):
+    label = models.CharField(max_length=100, null=False)
+    level = models.IntegerField(null=False)
+
+class Project(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 class Task(models.Model):
-    title = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    priority = models.ForeignKey(Priority, on_delete=models.CASCADE)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
     description = models.TextField()
